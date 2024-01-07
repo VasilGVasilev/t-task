19f3ced4c58c53cd2eac949726091cb05b9e60ef
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-07 17:44:32 +0200

    Create Navbar.jsx

---
+++ project/components/Navbar.jsx
@@ -0,0 +1,228 @@
+import { useRef, useState } from "react";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+
+
+import { motion } from "framer-motion"
+
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
+
+
+
+
+const container = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+};
+
+const navbarVariant = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
+};
+const LinkMobileTemplate = ({ urlLink, page, setIsMenuToggled }) => {
+    const location = useLocation()
+
+    return (
+        <Link
+            to={urlLink}
+            className={`${location.pathname === urlLink ? "text-[#00BAFC]" : "text-[#142D55]"
+                }  transition duration-300`}
+            onClick={() => {
+                setIsMenuToggled(false);
+            }
+            }
+        >
+
+            {page}
+        </Link>
+
+    );
+};
+
+const LinkTemplate = ({ urlLink, page }) => {
+    const location = useLocation();
+    return (
+        <Link
+            to={urlLink}
+            className={`${location.pathname == urlLink ? "text-[#00BAFC]" : "text-[#142D55]"
+                }  transition duration-300`}
+        >
+            {page}
+        </Link>
+    );
+};
+
+
+
+const Navbar = () => {
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+    const isDesktop = useMediaQuery("(min-width: 1120px)");
+
+    const modalRef = useRef(null);
+
+    const closeModal = (e) => {
+        e.stopPropagation();
+        if (e.target == modalRef.current) {
+            setIsMenuToggled(!isMenuToggled)
+        }
+    }
+
+    return (
+        // animation works only on Home page:
+        // the Navbar is fixed, meaning when we start the page, it is hidden and scroll makes it appear
+        // but also if we refresh down the Home page, due to Navbar also being fixed to viewport, it will appear since being fixed makes it also constanlty inView
+        <div
+        >
+            <nav className={`bg-[#166534] flex flex-col z-40 w-full fixed top-0 shadow-xl`} >
+
+                <div className="flex items-center justify-between mx-auto w-5/6">
+
+                    <Link className='saturate-200 duration-200 flex justify-center items-center h-14 w-20 sm:h-20 sm:w-32 text-xl 2xl:text-2xl text-black font-bold' to={'/'}>
+
+                            <img src="cgm.png" className="h-10 w-10 sm:h-16 sm:w-16 shadow-2xl rounded-full hover:scale-110 transition duration-300" alt="" />
+
+
+
+                    </Link>
+                    {/* DESKTOP NAV */}
+                    {isDesktop ? (
+                        <div className={`text-xl 2xl:text-2xl flex justify-between gap-10 2xl:gap-16 text-black font-bold`}>
+
+                            <LinkTemplate
+                                urlLink='/A11'
+                                page="A11"
+                            />
+                            <LinkTemplate
+                                urlLink='/A111'
+                                page="A111"
+                            />
+                            <LinkTemplate
+                                urlLink='/TB11'
+                                page="TB11"
+                            />
+                            <LinkTemplate
+                                urlLink='/TM8'
+                                page="TM8"
+                            />
+                            <LinkTemplate
+                                urlLink='/TM10'
+                                page="TM10"
+                            />
+                        </div>
+                    ) : (
+                        <button
+                            className="rounded-full text-black p-2"
+                            onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            aria-label="menu"
+                        >
+                            <GiHamburgerMenu size={24} />
+                        </button>
+                    )}
+
+                    {/* MOBILE MENU POPUP */}
+                    {!isDesktop && isMenuToggled && (
+                        <div
+                            className="fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-80"
+                            onClick={closeModal}
+                            ref={modalRef}
+                        >
+                            <motion.div
+                                className="fixed right-0 bottom-0 h-full bg-black w-[300px]"
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.1 }}
+                                transition={{ duration: 0.3 }}
+                                variants={{
+                                    hidden: { opacity: 0, x: 50 },
+                                    visible: { opacity: 1, x: 0 },
+                                }}
+                            >
+                                {/* CLOSE ICON */}
+                                <div className="flex justify-end p-12">
+                                    <button
+                                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                                        aria-label="close"
+                                    >
+                                        <AiOutlineClose size={24} className="text-neutral-600" />
+                                    </button>
+                                </div>
+
+                                {/* MENU ITEMS */}
+                                <motion.div
+                                    className="flex flex-col gap-10 ml-[33%] text-2xl text-white"
+                                    variants={container}
+                                    initial="hidden"
+                                    whileInView="visible"
+                                    viewport={{ once: true, amount: 0.2 }}
+                                >
+
+                                    <motion.div
+                                        variants={navbarVariant}
+                                        viewport={{ once: true }}
+                                    >
+                                        <LinkMobileTemplate
+                                            urlLink='/A11'
+                                            page="A11"
+                                            setIsMenuToggled={setIsMenuToggled}
+                                        />
+                                    </motion.div>
+                                    <motion.div
+                                        variants={navbarVariant}
+                                        viewport={{ once: true }}
+                                    >
+                                        <LinkMobileTemplate
+                                            urlLink='/A111'
+                                            page="A111"
+                                            setIsMenuToggled={setIsMenuToggled}
+                                        />
+                                    </motion.div>
+                                    <motion.div
+                                        variants={navbarVariant}
+                                        viewport={{ once: true }}
+                                    >
+
+                                        <LinkMobileTemplate
+                                            urlLink='/TB11'
+                                            page="TB11"
+                                            setIsMenuToggled={setIsMenuToggled}
+                                        />
+                                    </motion.div>
+                                    <motion.div
+                                        variants={navbarVariant}
+                                        viewport={{ once: true }}
+                                    >
+
+                                        <LinkMobileTemplate
+                                            urlLink='/TM8'
+                                            page="TM8"
+                                            setIsMenuToggled={setIsMenuToggled}
+                                        />
+                                    </motion.div>
+                                    <motion.div
+                                        variants={navbarVariant}
+                                        viewport={{ once: true }}
+                                    >
+
+                                        <LinkMobileTemplate
+                                            urlLink='/TM10'
+                                            page="TM10"
+                                            setIsMenuToggled={setIsMenuToggled}
+                                        />
+                                    </motion.div>
+                                </motion.div>
+                            </motion.div>
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
