54766c76c959b7ce0aef840ef146b3cb37a4f9eb
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-12 10:18:58 +0200

    transportData fetched via createAsyncThunk

--- project/src/store/sliceData.jsx
+++ project/src/store/sliceData.jsx
@@ -1,9 +1,16 @@
-import dataLines from '../../../DB/data.json'
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: dataLines,
+  posts: [],
+  status: 'idle',
+  error: null
 }
+
+export const fetchData = createAsyncThunk('data/fetchData', async () => {
+  const response = await import('../../../DB/data.json')
+  // .default, not .data  due to the DB being default export
+  return response.default
+})
 
 export const counterSlice = createSlice({
   name: 'data',
@@ -11,6 +18,21 @@ export const counterSlice = createSlice({
   reducers: {
     // no need for reducers, no data fetching
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
 })
 
 // Action creators are generated for each case reducer function
