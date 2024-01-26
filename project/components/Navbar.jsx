24147967085a8c24321f05a3b3aae36bf2985b8e
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2024-01-26 17:53:55 +0200

    css based navbar

--- project/components/Navbar.jsx
+++ project/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
     const colors = useSelector((state) => state.colors.colors)
     const colorsStatus = useSelector(state => state.colors.status)
     const errorColors = useSelector(state => state.colors.error)
-    
+
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isDesktop = useMediaQuery("(min-width: 1120px)");
 
@@ -83,10 +83,11 @@ const Navbar = () => {
 
     let content
 
-    if ( colorsStatus === 'loading') {
+    if (colorsStatus === 'loading') {
         content = <div>Loading...</div>
-    } else if ( colorsStatus === 'succeeded') {
+    } else if (colorsStatus === 'succeeded') {
         // Sort posts in reverse chronological order by datetime string
+        const showMobileMenu = isMenuToggled ? 'block' : 'hidden'
         return (
             <nav className={`bg-white flex flex-col z-40 w-full shadow-lg`} >
 
@@ -100,129 +101,124 @@ const Navbar = () => {
 
                     </Link>
                     {/* DESKTOP NAV */}
-                    {isDesktop ? (
-                        <div className={`text-xl 2xl:text-2xl flex justify-between gap-10 2xl:gap-16 font-semibold`}>
+                    <div className={`hidden text-xl 2xl:text-2xl md:flex justify-between gap-10 2xl:gap-16 font-semibold`}>
 
-                            <LinkTemplate
-                                urlLink='/line/A11'
-                                page="A11"
-                                lineColor={colors.A11}
-                            />
-                            <LinkTemplate
-                                urlLink='/line/A111'
-                                page="A111"
-                                lineColor={colors.A111}
-                            />
-                            <LinkTemplate
-                                urlLink='/line/TB11'
-                                page="TB11"
-                                lineColor={colors.TB11}
-                            />
-                            <LinkTemplate
-                                urlLink='/line/TM8'
-                                page="TM8"
-                                lineColor={colors.TM8}
-                            />
-                            <LinkTemplate
-                                urlLink='/line/TM10'
-                                page="TM10"
-                                lineColor={colors.TM10}
-                            />
-                        </div>
-                    ) : (
-                        <button
-                            className="rounded-full text-black p-2"
-                            onClick={() => setIsMenuToggled(!isMenuToggled)}
-                            aria-label="menu"
-                        >
-                            <GiHamburgerMenu size={24} />
-                        </button>
-                    )}
+                        <LinkTemplate
+                            urlLink='/line/A11'
+                            page="A11"
+                            lineColor={colors.A11}
+                        />
+                        <LinkTemplate
+                            urlLink='/line/A111'
+                            page="A111"
+                            lineColor={colors.A111}
+                        />
+                        <LinkTemplate
+                            urlLink='/line/TB11'
+                            page="TB11"
+                            lineColor={colors.TB11}
+                        />
+                        <LinkTemplate
+                            urlLink='/line/TM8'
+                            page="TM8"
+                            lineColor={colors.TM8}
+                        />
+                        <LinkTemplate
+                            urlLink='/line/TM10'
+                            page="TM10"
+                            lineColor={colors.TM10}
+                        />
+                    </div>
+                    <button
+                        className="block md:hidden rounded-full text-black p-2"
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        aria-label="menu"
+                    >
+                        <GiHamburgerMenu size={24} />
+                    </button>
 
                     {/* MOBILE MENU POPUP */}
-                    {!isDesktop && isMenuToggled && (
-                        <div
-                            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80"
-                            onClick={closeModal}
-                            ref={modalRef}
+                    <div
+                        className={`${showMobileMenu} fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80`}
+                        onClick={closeModal}
+                        ref={modalRef}
+                    >
+                        <motion.div
+                            className="fixed right-0 bottom-0 h-full bg-black w-[340px]"
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.1 }}
+                            transition={{ duration: 0.3 }}
+                            variants={{
+                                hidden: { opacity: 0, x: 50 },
+                                visible: { opacity: 1, x: 0 },
+                            }}
                         >
+                            {/* CLOSE ICON */}
+                            <div className="flex justify-end p-12">
+                                <button
+                                    onClick={() => setIsMenuToggled(!isMenuToggled)}
+                                    aria-label="close"
+                                >
+                                    <AiOutlineClose size={24} className="text-neutral-600" />
+                                </button>
+                            </div>
+
+                            {/* MENU ITEMS */}
                             <motion.div
-                                className="fixed right-0 bottom-0 h-full bg-black w-[340px]"
+                                className="flex flex-col gap-10 ml-[33%] text-2xl text-white"
                                 initial="hidden"
                                 whileInView="visible"
-                                viewport={{ once: true, amount: 0.1 }}
-                                transition={{ duration: 0.3 }}
-                                variants={{
-                                    hidden: { opacity: 0, x: 50 },
-                                    visible: { opacity: 1, x: 0 },
-                                }}
+                                viewport={{ once: true, amount: 0.2 }}
                             >
-                                {/* CLOSE ICON */}
-                                <div className="flex justify-end p-12">
-                                    <button
-                                        onClick={() => setIsMenuToggled(!isMenuToggled)}
-                                        aria-label="close"
-                                    >
-                                        <AiOutlineClose size={24} className="text-neutral-600" />
-                                    </button>
-                                </div>
-
-                                {/* MENU ITEMS */}
-                                <motion.div
-                                    className="flex flex-col gap-10 ml-[33%] text-2xl text-white"
-                                    initial="hidden"
-                                    whileInView="visible"
-                                    viewport={{ once: true, amount: 0.2 }}
-                                >
 
 
-                                    <LinkMobileTemplate
-                                        urlLink='/line/A11'
-                                        page="A11"
-                                        setIsMenuToggled={setIsMenuToggled}
-                                        lineColor={colors.A11}
-                                    />
+                                <LinkMobileTemplate
+                                    urlLink='/line/A11'
+                                    page="A11"
+                                    setIsMenuToggled={setIsMenuToggled}
+                                    lineColor={colors.A11}
+                                />
 
 
-                                    <LinkMobileTemplate
-                                        urlLink='/line/A111'
-                                        page="A111"
-                                        setIsMenuToggled={setIsMenuToggled}
-                                        lineColor={colors.A111}
-                                    />
+                                <LinkMobileTemplate
+                                    urlLink='/line/A111'
+                                    page="A111"
+                                    setIsMenuToggled={setIsMenuToggled}
+                                    lineColor={colors.A111}
+                                />
 
-                                    <LinkMobileTemplate
-                                        urlLink='/line/TB11'
-                                        page="TB11"
-                                        setIsMenuToggled={setIsMenuToggled}
-                                        lineColor={colors.TB11}
+                                <LinkMobileTemplate
+                                    urlLink='/line/TB11'
+                                    page="TB11"
+                                    setIsMenuToggled={setIsMenuToggled}
+                                    lineColor={colors.TB11}
 
-                                    />
-
-
-                                    <LinkMobileTemplate
-                                        urlLink='/line/TM8'
-                                        page="TM8"
-                                        setIsMenuToggled={setIsMenuToggled}
-                                        lineColor={colors.TM8}
-
-                                    />
+                                />
 
 
-                                    <LinkMobileTemplate
-                                        urlLink='/line/TM10'
-                                        page="TM10"
-                                        setIsMenuToggled={setIsMenuToggled}
-                                        lineColor={colors.TM10}
-                                    />
-                                </motion.div>
+                                <LinkMobileTemplate
+                                    urlLink='/line/TM8'
+                                    page="TM8"
+                                    setIsMenuToggled={setIsMenuToggled}
+                                    lineColor={colors.TM8}
+
+                                />
+
+
+                                <LinkMobileTemplate
+                                    urlLink='/line/TM10'
+                                    page="TM10"
+                                    setIsMenuToggled={setIsMenuToggled}
+                                    lineColor={colors.TM10}
+                                />
                             </motion.div>
-                        </div>
-                    )}
+                        </motion.div>
+                    </div>
                 </div>
             </nav>
         );
-    } else if ( colorsStatus === 'failed') {
+    } else if (colorsStatus === 'failed') {
         content = <div>{errorColors}</div>
     }
 
